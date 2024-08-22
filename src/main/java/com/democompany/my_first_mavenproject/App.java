e1d9eacbd617332390832b5094260ff2f0d609bb
Bharath <bigb8500687798@gmail.com>
2024-08-22 15:31:52 +0530

    This is my maven deployment

---
+++ src/main/java/com/democompany/my_first_mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.democompany.my_first_mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
